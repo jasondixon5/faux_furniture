54fda1b29c766474a46fd79c2796c898ac1c1a72
Jason Dixon <jasondixonmail@gmail.com>
2026-02-20 14:01:48 -0800

    Initiated django project Made various organizing changes

--- faux_furniture/settings.py
+++ faux_furniture/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+	"faux_furniture.apps.accounts",
+	"faux_furniture.apps.core",
 ]
 
 MIDDLEWARE = [
